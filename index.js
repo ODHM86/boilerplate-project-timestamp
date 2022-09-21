900dcf50f138a2505168185060e7a15e4bb8623d
ODHM86 <67576282+ODHM86@users.noreply.github.com>
2022-09-20 22:43:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var express = require('express');
 var app = express();
 
 console.log('date api');
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -18,24 +19,42 @@ app.use(express.static('public'));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-
+app.get("/api", function(req, res, next) {
+  console.log(req.params);
+  req.time = new Date().toUTCString();
+ 
+  let unix = new Date(req.time).valueOf();
+  res.json({"unix": unix, "utc":  req.time});
+  console.log("api");
+  res.json({json: "api"});
+//  console.log("get to /api, params: ",req.params);
+}
+        );
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+
 app.use("/api/:date", function (req, res) {
+  console.log("inside /api/:date");
   console.log("use",req.params);
   let date_string = req.params.date;
-  
+  let date_string_s = new Date(parseInt(date_string)).toUTCString();
+  console.log("type of date_string: ", typeof date_string);
   console.log("length", date_string.length)
+  console.log("is invalid date?: ",Date(date_string));
+  if(date_string_s == 'Invalid Date'){
+    res.json({error:date_string_s});
+  }
+  else{
   if(date_string.length == 13 ) {
-    let date_string_time = new Date(date_string*1000);
-    console.log(date_string_time);
- date_string_time.toUTCString();
+    let date_string_time = new Date(parseInt(date_string)).toUTCString();
+    console.log("from unix to utc: ",date_string_time);
+// date_string_time.toUTCString();
     let unix_date = date_string;
-  res.json([{unix: date_string},{date: date_string_time.toUTCString()}]);
+  res.json({unix: parseInt(date_string), utc: date_string_time});
   
   }
  // Date(date_string).toUTCString();
@@ -46,15 +65,13 @@ app.use("/api/:date", function (req, res) {
   let unix = new Date(date_string).valueOf();
     console.log("unix time ",unix);
     
-    let dates_
+    
     console.log("utc",dates_utc)
-  res.json([{date: dates_utc.toUTCString()},{unix: unix}]);
+  res.json({unix: unix, utc:  dates_utc.toUTCString()});
   }
+    }
 });
 
-app.get("/api"), function(req, res, next) {
-  console.log("get to /api, params: ",req.params);
-}
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
