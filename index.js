fa7fe92d909abe153a1bc9f2714131e6933b3d12
odhm86 <67576282+ODHM86@users.noreply.github.com>
2022-09-21 20:37:13 +0000

    final version

--- index.js
+++ index.js
@@ -32,9 +32,12 @@ app.get("/api", function(req, res, next) {
         );
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+
+
+  app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
 
 
 app.use("/api/:date", function (req, res) {
