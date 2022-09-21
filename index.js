5711c31a10d5cdfe971eb263cb8c0be729f4fa2c
ODHM86 <67576282+ODHM86@users.noreply.github.com>
2022-09-20 20:18:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 var express = require('express');
 var app = express();
 
+console.log('date api');
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -24,9 +25,40 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.use("/api/:date", function (req, res) {
+  console.log("use",req.params);
+  let date_string = req.params.date;
+  
+  console.log("length", date_string.length)
+  if(date_string.length == 13 ) {
+    let date_string_time = new Date(date_string*1000);
+    console.log(date_string_time);
+ date_string_time.toUTCString();
+    let unix_date = date_string;
+  res.json([{unix: date_string},{date: date_string_time.toUTCString()}]);
+  
+  }
+ // Date(date_string).toUTCString();
+ // res.json({date:})
+  else {
+    let dates_utc = new Date(date_string);
 
+  let unix = new Date(date_string).valueOf();
+    console.log("unix time ",unix);
+    
+    let dates_
+    console.log("utc",dates_utc)
+  res.json([{date: dates_utc.toUTCString()},{unix: unix}]);
+  }
+});
+
+app.get("/api"), function(req, res, next) {
+  console.log("get to /api, params: ",req.params);
+}
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
